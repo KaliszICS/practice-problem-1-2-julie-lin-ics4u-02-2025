3d6bc6e206378847986da051aaa2a75ae8d569ae
Julie Lin <julie.lin@student.tdsb.on.ca>
2025-03-05 01:39:01 +0000

    1.1 is breaking down on me.

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,7 +3,31 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
-
-	
-
+	public static boolean validIndex(int[] arr, int num){
+		try {
+			int temp = arr[num];
+			return true;
+		}
+		catch (ArrayIndexOutOfBoundsException e) {
+			return false;
+		}
+	}
+	public static int divide (int num1, int num2){
+		try{
+			int temp = num1/num2;
+			return temp;
+		}
+		catch (ArithmeticException e){
+			return 0; 
+		}
+	}
+	public static int safeConvertStringtoInt (String str){
+		try {
+			int num = Integer.parseInt(str);
+			return num;
+		}
+		catch (NumberFormatException e) {
+			return 0;
+		}
+	}
 }
